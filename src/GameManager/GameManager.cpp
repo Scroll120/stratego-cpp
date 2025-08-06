f55123795a823d76a25fb3f19cb1a16aed176e07
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-08-06 17:27:57 +0200

    feat: implement GameManager class

--- src/GameManager/GameManager.cpp
+++ src/GameManager/GameManager.cpp
@@ -1,58 +1,125 @@
 #include "GameManager.h"
-#include <iostream>
+#include "../pieces/flag.h"
+#include "../pieces/bomb.h"
+#include "../pieces/spy.h"
+#include "../pieces/scout.h"
+#include "../pieces/miner.h"
+#include "../pieces/normalPiece.h"
 
-GameManager::GameManager() : /*currentPlayer(nullptr), board(nullptr)*/isRunning(true)/*, player1(nullptr), player2(nullptr)*/ {
+GameManager::GameManager() : player1(nullptr), player2(nullptr), currentPlayer(nullptr), board(nullptr),
+                             isRunning(true), gameInitialized(false) {
     initializeGame();
 }
 
 GameManager::~GameManager() {
-    // delete player1;
-    // delete player2;
-    // delete board;
+    delete player1;
+    delete player2;
+    delete board;
 }
 
 void GameManager::initializeGame() {
-    // player1 = new Player();
-    // player2 = new Player();
+    if (gameInitialized) return;
 
-    // currentPlayer = player1;
+    player1 = new Player(PlayerID::PLAYER1);
+    player2 = new Player(PlayerID::PLAYER2);
+    currentPlayer = player1;
 
-    // board = new Board();
-    // board->initialize();  I gues this will be the method??
+    board = new Board();
+    setupBoard();
+
+    setupPiecesForPlayer(player1, true);
+    setupPiecesForPlayer(player2, false);
+
+    gameInitialized = true;
+}
+
+void GameManager::setupPiecesForPlayer(Player *player, bool isPlayer1) {
+    player->addPiece(new Flag());
+
+    for (int i = 0; i < 6; i++) {
+        player->addPiece(new Bomb());
+    }
+
+    player->addPiece(new Spy());
+
+    for (int i = 0; i < 8; i++) {
+        player->addPiece(new scout);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        player->addPiece(new Miner());
+    }
+
+    int normalPieceCounts[] = {4, 4, 4, 3, 2, 1};
+    for (int rank = 4; rank <= 9; rank++) {
+        for (int i = 0; i < normalPieceCounts[rank - 4]; i++) {
+            player->addPiece(new NormalPiece(rank));
+        }
+    }
+}
+
+void GameManager::setupBoard() {
+    board->addObstacle(4, 2);
+    board->addObstacle(4, 3);
+    board->addObstacle(4, 6);
+    board->addObstacle(4, 7);
+    board->addObstacle(5, 2);
+    board->addObstacle(5, 3);
+    board->addObstacle(5, 6);
+    board->addObstacle(5, 7);
 }
 
 bool GameManager::checkWin() {
-    //check if the opponent's flag has been captured
-    //or if all movable piecas are captured
-    // bool player1Wins = somelogic;
-    // bool player2Wins = somelogic;
-
-    if (true/*player1Wins || player2Wins*/) {
-        isRunning = false;
+    if (currentPlayer == player1 && player2->isFlagCaptured()) {
         return true;
     }
+    if (currentPlayer == player2 && player1->isFlagCaptured()) {
+        return true;
+    }
+
+    if (currentPlayer == player1 && !player2->hasMoveablePiece()) {
+        return true;
+    }
+    if (currentPlayer == player2 && !player1->hasMoveablePiece()) {
+        return true;
+    }
+
     return false;
 }
 
 void GameManager::updateGameState() {
     if (checkWin()) {
+        isRunning = false;
         return;
     }
-    //update other game states
 }
 
 void GameManager::switchPlayer() {
-    // currentPlayer = (currentPlayer == player1) ? player2 : player1;
+    currentPlayer = (currentPlayer == player1) ? player2 : player1;
 }
 
-bool GameManager::validateMove(/*move parameters*/) {
-    //validate if the move is legal
-    //check if the piece belongs to current player
-    //check if destination is valid
-    return true; //placeholder just like everyting else
+bool GameManager::validateMove(int fromX, int fromY, int toX, int toY) {
+    if (fromX < 0 || fromX >= 10 || fromY < 0 || fromY >= 10 || toX < 0 || toX >= 10 || toY < 0 || toY >= 10) {
+        return false;
+    }
+
+    Piece* piece = board->getPiece(fromX, fromY);
+    if (!piece || (piece->getRank() == 11 || piece->getRank() == 12)) {
+        return false;
+    }
+
+    return board->isMoveValid(fromX, fromY, toX, toY);
 }
 
+bool GameManager::attemptMove(int fromX, int fromY, int toX, int toY) {
+    if (!validateMove(fromX, fromY, toX, toY)) {
+        return false;
+    }
 
+    bool moveSuccess = board->movePiece(fromX, fromY, toX, toY);
+    if (moveSuccess) {
+        switchPlayer();
+    }
 
-
-
+    return moveSuccess;
+}
