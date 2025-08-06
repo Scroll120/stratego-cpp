a80744655d63f6bc694b06193169d82b2bb534c7
SzekelyBoti <szekelyboti1@gmail.com>
2025-08-06 09:51:09 +0200

    Player class implementation

---
+++ src/game/Player.h
@@ -0,0 +1,24 @@
+﻿#ifndef PLAYER_H
+#define PLAYER_H
+#include <vector>
+
+enum class PlayerID { PLAYER1 = 1, PLAYER2 = 2 };
+
+class Piece;
+class Player {
+private:
+  PlayerID id;
+  std::vector<Piece *> pieces;
+  static constexpr int MAX_PIECES = 40;
+public:
+  Player(PlayerID id) : id(id) {}
+  PlayerID getId() const { return id; }
+  bool addPiece(Piece* piece);
+  const std::vector<Piece*>& getPieces() const { return pieces; }
+  int getMaxSize() const { return MAX_PIECES; };
+  bool removePiece(Piece* piece);
+  bool isFlagCaptured();
+  bool hasMoveablePiece();
+};
+
+#endif //PLAYER_H
